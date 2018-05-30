354ca8975d7c1bc737073ae8621eae0a664801d5
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-05-30 10:09:01 -0500

    printing out a varible

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,12 @@
 public class hello {
     public static void main(String[] args) {
+
+
         System.out.println("Hello, Ian!");
+
+        int myFirstNumber = 5;
+        /* datatype variable = value */
+
+        System.out.println("My first number value, "+ myFirstNumber);
     }
 }
