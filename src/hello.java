6b214662e46f1535d6cb389ea9eadbd2f29b3e02
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-05-30 10:14:16 -0500

    fun with int variable values

--- src/hello.java
+++ src/hello.java
@@ -4,9 +4,15 @@ public class hello {
 
         System.out.println("Hello, Ian!");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10+ 5) + (2*10);
         /* datatype variable = value */
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
 
-        System.out.println("My first number value, "+ myFirstNumber);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastNumber = 1000 - myTotal;
+
+        System.out.println("My last number value, "+ myLastNumber);
     }
 }
